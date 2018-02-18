79d4e02309818b0c67e1eef5fd784d7b0cd661c9
School <school@MacBook.local>
2018-02-18 11:34:36 -0500

    code

---
+++ primepantry.py
@@ -0,0 +1,9 @@
+# primepantry.py
+# Authors: Logan Swanson, Maddy Fox, Miriam Hodas
+# Due: February 22 2018
+# Takes in a list of items and their positive integer weights,
+# number of items, and total weight
+# Returns a subset of items with weight equal to total weight
+
+def primePantry(lst, numItems, weight):
+    
