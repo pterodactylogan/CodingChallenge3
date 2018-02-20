6033a3dba4011ff80db021b96fa5b6a59170b958
Maddy Fox <Maddy@MacBook-Pro.local>
2018-02-20 17:00:49 -0500

    fixed repeat and pre-loading issues

--- primePantryMDF.py
+++ primePantryMDF.py
@@ -1,48 +1,48 @@
-# File: primePantryMDF.py
-# Great Python Race: Prime Pantry Problem
-# Author: Maddy Fox
-
-#import random
+# File: primepantryMDF.py
+# Challenge 3: Prime Pantry v2
+# Authors: Maddy Fox, Miriam Hodas, Logan Swanson
 
 def prime_pantry(itemDict, nItems, totalWt):
-    #invDict = {v:k for k,v in itemDict.items()}
+    """Given a dictionary of items and their prices, size of that dictionary,
+    and a weight value, returns a list of items that adds up to that weight if
+    possible or a message notifying the user that the weight isn't achievable"""
     knownResults = [ "" for e in range(totalWt+1)]
 
-    for key in itemDict:
-      knownResults[itemDict[key]] = key
-    
     # Loop over all weight values, in order
     for wt in range(1, totalWt+1):
-      #print("weight: ", wt)
         
       # Check against each item weight
       for item in [key for key in itemDict if itemDict[key] <= wt]:
-        #print("item: ", item)
             
-        # If using the current item lets us
-        # reuse a prior result, just update!
-        if knownResults[wt-itemDict[item]] != "":
-          print("last result for ", wt, " - ", item, " : ", knownResults[wt-itemDict[item]])
+        # If using the current item lets us reuse a prior result, just update!
+        if knownResults[wt-itemDict[item]] != "" or wt-itemDict[item] == 0:
 
-          #only update if no items repeated
-          if item not in knownResults[wt-itemDict[item]]:
-            print("no repeats")
+          # Only update if no items repeated
+          if item not in get_items(itemDict, knownResults, wt-itemDict[item]):
             knownResults[wt] = item
-            #once solution found, don't need to check others
-            break
             
-        #print("known result for ", wt, " : ", knownResults[wt])
-        
-    finalLst = []
-    n = totalWt
-    while n > 0:
-      finalLst.append(knownResults[n])
-      n -= itemDict[knownResults[n]]
+            # Once one solution found, don't need to find others
+            break
 
-    return finalLst
+    if knownResults[totalWt]:
+        return get_items(itemDict, knownResults, totalWt)
+    else:
+        return "No combination of items adds up to ", totalWt
+
+def get_items(itemDict, knownResults, n):
+    """Given a weight, returns a list of the items that add up to the weight"""
+    lst = []
+    
+    while n > 0:
+      lst.append(knownResults[n])
+      n -= itemDict[knownResults[n]]
+      
+    return lst
 
 def main():
   items = {"pepsi":55,"detergent":30, "chips":25, "cereal":15}
-  print(prime_pantry(items, len(items), 100))
-
+  wt = 100
+  #print(prime_pantry(sys.argv[0], sys.argv[1], sys.argv[2]))
+  print(prime_pantry(items, len(items), wt))
+  
 main()
